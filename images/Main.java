5b5d318bf8ca16898d467e51b5b2660f0cfb14a0
Brindha Krishnan <praneshkrishnan5@gmail.com>
2024-11-11 23:51:02 +0530

    new changes

---
+++ images/Main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args) {
+        String str="pranesh";String out=" ";
+        char ch;
+        for(int i=0;i<str.length();i++){
+            ch=str.charAt(i);
+            out=ch+out;
+        }
+            System.out.print(out);
+
+        
+    }
+}
